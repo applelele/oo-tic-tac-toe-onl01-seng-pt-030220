b95cadc9d0f03f66e6b56962176dc08dd4c4f740
YUKIKO IKEDA <applelele@gmail.com>
2020-03-27 03:21:33 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -102,4 +102,17 @@ WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8
     end
   end
 
+  def play
+    until over?
+      turn
+    end
+
+    if winner
+      puts "Congratulation, #{@winner}!"
+    elsif draw?
+      puts "The game is over"
+    end
+
+  end
+
 end
