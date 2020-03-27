7f3ad2d8ba53bbcf867018acf5a4eeff20bb1bdd
YUKIKO IKEDA <applelele@gmail.com>
2020-03-27 03:11:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,7 +35,7 @@ WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8
   end
 
   def valid_move?(index)
-    if position_taken?(index) || index > 8 # 1.that is not taken, 2.index has to be between 0-8
+    if position_taken?(index) || index > 8 || index < 0 # 1.that is not taken, 2.index has to be between 0-8
       false
     else
       true
